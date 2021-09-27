c72de9cf9505a694ae77e7164b13f8e00b56bac6
Finegorko <Finegorko@gmail.com>
2021-09-27 13:31:45 +0300

    go

--- g2g_antonidas.js
+++ g2g_antonidas.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         G2G_Antonidas
 // @namespace    http://g2g.com/
-// @version      0.2
+// @version      0.01
 // @author       Finegorko
 // @include      https://www.g2g.com/sell/manage?region=41709&service=1&game=2299&type=0&sorting=title%40asc
 // @include      https://www.g2g.com/offer/Antonidas--DE*
