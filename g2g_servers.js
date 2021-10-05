d310e018db749f3a6c6d33c0913d9c62fcd39711
Finegorko <Finegorko@gmail.com>
2021-10-05 23:09:28 +0300

    0.75

--- g2g_servers.js
+++ g2g_servers.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         G2G Auto Dump Prices: Antonidas, Argent Dawn, Ravencrest
 // @namespace    http://g2g.com/
-// @version      0.71
+// @version      0.75
 // @author       Finegorko
 // @include      https://www.g2g.com/sell/manage?region=41709&service=1&game=2299&type=0&sorting=title%40asc
 // @include      https://www.g2g.com/offer/Antonidas*
@@ -16,137 +16,124 @@
     let ArgentDawn_pathname = "/offer/Argent-Dawn--EU----Alliance"
     let Ravencrest_pathname = "/offer/Ravencrest--EU----Alliance"
     let Listing_pathname = "?region=41709&service=1&game=2299&type=0&sorting=title%40asc"
-    // prices for Antonidas, Argent Dawn, Ravencrest
+    // check prices: Antonidas - Argent Dawn - Ravencrest
     if (location.pathname == Antonidas_pathname) {
         console.log("Server: Antonidas (EU)")
         document.querySelector(
             ".title_top-offers.other_seller_header"
         ).textContent = "Server: Antonidas (EU)";
-        function AntonidasCheck() {
-            let y = 1; // price N
-            let i = 1; // stock N
-            let done = 0; // while
-            let s = 2; // seller name
-            function Stop() {
-                done = done + 1;
-            }
-            while (done < 1) {
-                let Stock = document.querySelectorAll(
-                    ".offers-bottom-attributes.offer__content-lower-items"
-                )[i].innerText;
-                let Stock_N = Stock.replace(/[^0-9]/g, "");
-                let SellerName = document.querySelectorAll(".seller__name-detail")[s]
+        let y = 1; // price N
+        let i = 1; // stock N
+        let done = 0; // while
+        let s = 2; // seller name
+        function Stop() {
+            done = done + 1;
+        }
+        while (done < 1) {
+            let Stock = document.querySelectorAll(
+                ".offers-bottom-attributes.offer__content-lower-items"
+            )[i].innerText;
+            let Stock_N = Stock.replace(/[^0-9]/g, "");
+            let SellerName = document.querySelectorAll(".seller__name-detail")[s]
+                .textContent;
+            if (SellerName == "FineGold") {
+                console.log("Не могу перебить самого себя.");
+                Stop();
+            } else if (Stock_N < 500) {
+                console.log(
+                    Stock_N,
+                    "тысяч",
+                    "меньше чем 500 тысяч, перехожу к следующему..."
+                );
+                y = y + 1;
+                i = i + 3;
+                s = s + 1;
+            } else {
+                console.log(Stock_N, "тысяч", "больше чем 500 тысяч, меняю цену...");
+                let lowest_price = document.querySelectorAll(".offer-price-amount")[y]
                     .textContent;
-                if (SellerName == "FineGold") {
-                    console.log("Не могу перебить самого себя.");
-                    Stop();
-                } else if (Stock_N < 500) {
-                    console.log(
-                        Stock_N,
-                        "тысяч",
-                        "меньше чем 500 тысяч, перехожу к следующему..."
-                    );
-                    y = y + 1;
-                    i = i + 3;
-                    s = s + 1;
-                } else {
-                    console.log(Stock_N, "тысяч", "больше чем 500 тысяч, меняю цену...");
-                    let lowest_price = document.querySelectorAll(".offer-price-amount")[y]
-                        .textContent;
-                    let dumping = 0.000001;
-                    let Antonidas_Price = lowest_price - dumping;
-                    localStorage.Antonidas_Price = Antonidas_Price.toFixed(6);
-                    console.log("Текущая цена -", lowest_price);
-                    console.log("Меняю цену на -", Antonidas_Price.toFixed(6));
-                    Stop();
-                }
+                let dumping = 0.000001;
+                let Antonidas_Price = lowest_price - dumping;
+                localStorage.Antonidas_Price = Antonidas_Price.toFixed(6);
+                console.log("Текущая цена -", lowest_price);
+                console.log("Меняю цену на -", Antonidas_Price.toFixed(6));
+                Stop();
             }
         }
     }
     if (location.pathname == ArgentDawn_pathname) {
         console.log("Server: Argent Dawn (EU)")
         document.querySelector('.title_top-offers.other_seller_header').textContent = "Server: Argent Dawn (EU)";
-        function ArgentDawnCheck() {
-            let y = 1; // price N
-            let i = 1; // stock N
-            let done = 0 // while
-            let s = 2; // seller name
-            function Stop() {
-                done = done + 1;
-            }
-            while (done < 1) {
-                let Stock = document.querySelectorAll('.offers-bottom-attributes.offer__content-lower-items')[i].innerText;
-                let Stock_N = Stock.replace(/[^0-9]/g, "");
-                let SellerName = document.querySelectorAll('.seller__name-detail')[s].textContent;
-                if (SellerName == "FineGold") {
-                    console.log("Не могу перебить самого себя.")
-                    Stop();
-                } else if (Stock_N < 500) {
-                    console.log(Stock_N, "тысяч", 'меньше чем 500 тысяч, перехожу к следующему...')
-                    y = y + 1;
-                    i = i + 3;
-                    s = s + 1;
-                } else {
-                    console.log(Stock_N, "тысяч", 'больше чем 500 тысяч, меняю цену...')
-                    let lowest_price = document.querySelectorAll('.offer-price-amount')[y].textContent;
-                    let dumping = 0.000001;
-                    let ArgentDawn_Price = lowest_price - dumping;
-                    localStorage.ArgentDawn_Price = ArgentDawn_Price.toFixed(6);
-                    console.log("Текущая цена -", lowest_price);
-                    console.log("Меняю цену на -", ArgentDawn_Price.toFixed(6));
-                    Stop();
-                }
+        let y = 1; // price N
+        let i = 1; // stock N
+        let done = 0 // while
+        let s = 2; // seller name
+        function Stop() {
+            done = done + 1;
+        }
+        while (done < 1) {
+            let Stock = document.querySelectorAll('.offers-bottom-attributes.offer__content-lower-items')[i].innerText;
+            let Stock_N = Stock.replace(/[^0-9]/g, "");
+            let SellerName = document.querySelectorAll('.seller__name-detail')[s].textContent;
+            if (SellerName == "FineGold") {
+                console.log("Не могу перебить самого себя.")
+                Stop();
+            } else if (Stock_N < 500) {
+                console.log(Stock_N, "тысяч", 'меньше чем 500 тысяч, перехожу к следующему...')
+                y = y + 1;
+                i = i + 3;
+                s = s + 1;
+            } else {
+                console.log(Stock_N, "тысяч", 'больше чем 500 тысяч, меняю цену...')
+                let lowest_price = document.querySelectorAll('.offer-price-amount')[y].textContent;
+                let dumping = 0.000001;
+                let ArgentDawn_Price = lowest_price - dumping;
+                localStorage.ArgentDawn_Price = ArgentDawn_Price.toFixed(6);
+                console.log("Текущая цена -", lowest_price);
+                console.log("Меняю цену на -", ArgentDawn_Price.toFixed(6));
+                Stop();
             }
         }
     }
     if (location.pathname == Ravencrest_pathname) {
         console.log("Server: Ravencrest (EU)")
         document.querySelector('.title_top-offers.other_seller_header').textContent = "Server: Ravencrest (EU)";
-        function RavencrestCheck() {
-            let y = 1; // price N
-            let i = 1; // stock N
-            let done = 0 // while
-            let s = 2; // seller name
-            function Stop() {
-                done = done + 1;
-            }
-            while (done < 1) {
-                let Stock = document.querySelectorAll('.offers-bottom-attributes.offer__content-lower-items')[i].innerText;
-                let Stock_N = Stock.replace(/[^0-9]/g, "");
-                let SellerName = document.querySelectorAll('.seller__name-detail')[s].textContent;
-                if (SellerName == "FineGold") {
-                    console.log("Не могу перебить самого себя.")
-                    Stop();
-                } else if (Stock_N < 500) {
-                    console.log(Stock_N, "тысяч", 'меньше чем 500 тысяч, перехожу к следующему...')
-                    y = y + 1;
-                    i = i + 3;
-                    s = s + 1;
-                } else {
-                    console.log(Stock_N, "тысяч", 'больше чем 500 тысяч, меняю цену...')
-                    let lowest_price = document.querySelectorAll('.offer-price-amount')[y].textContent;
-                    let dumping = 0.000001;
-                    let Ravencrest_Price = lowest_price - dumping;
-                    localStorage.Ravencrest_Price = Ravencrest_Price.toFixed(6);
-                    console.log("Текущая цена -", lowest_price);
-                    console.log("Меняю цену на -", Ravencrest_Price.toFixed(6));
-                    Stop();
-                }
+        let y = 1; // price N
+        let i = 1; // stock N
+        let done = 0 // while
+        let s = 2; // seller name
+        function Stop() {
+            done = done + 1;
+        }
+        while (done < 1) {
+            let Stock = document.querySelectorAll('.offers-bottom-attributes.offer__content-lower-items')[i].innerText;
+            let Stock_N = Stock.replace(/[^0-9]/g, "");
+            let SellerName = document.querySelectorAll('.seller__name-detail')[s].textContent;
+            if (SellerName == "FineGold") {
+                console.log("Не могу перебить самого себя.")
+                Stop();
+            } else if (Stock_N < 500) {
+                console.log(Stock_N, "тысяч", 'меньше чем 500 тысяч, перехожу к следующему...')
+                y = y + 1;
+                i = i + 3;
+                s = s + 1;
+            } else {
+                console.log(Stock_N, "тысяч", 'больше чем 500 тысяч, меняю цену...')
+                let lowest_price = document.querySelectorAll('.offer-price-amount')[y].textContent;
+                let dumping = 0.000001;
+                let Ravencrest_Price = lowest_price - dumping;
+                localStorage.Ravencrest_Price = Ravencrest_Price.toFixed(6);
+                console.log("Текущая цена -", lowest_price);
+                console.log("Меняю цену на -", Ravencrest_Price.toFixed(6));
+                Stop();
             }
         }
     }
-    // start check prices
-    if (location.pathname == Antonidas_pathname) {
-        AntonidasCheck()
-    } else if (location.pathname == ArgentDawn_pathname) {
-        ArgentDawnCheck()
-    } else if (location.pathname == Ravencrest_pathname) {
-        RavencrestCheck()
-    }
     // update prices
-    function UpdatePrices() {
-        if (location.search == Listing_pathname) {
-            console.log("Обнаружен Manage Listing, начинаю обновлять цены...")
+    if (location.pathname == Listing_pathname) {
+        clearTimeout()
+        console.log("Обнаружен Manage Listing, начинаю обновлять цены...")
+        function UpdatePrices() {
             function AntonidasInput() {
                 let priceListing = document.querySelectorAll('.g2g_products_price.editable.editable-click')[0].textContent;
                 let priceLocal = localStorage.Antonidas_Price;
@@ -216,33 +203,31 @@
                     document.getElementById('c2c_9003201').querySelector('.g2g_products_price.editable.editable-click').click();
                 }
             }
-            // таймер на выставление
-            function UpdateListings() {
-                setTimeout(AntonidasTable, 7000);
-                setTimeout(AntonidasInput, 7500);
-                setTimeout(ArgentDawnTable, 9000);
-                setTimeout(ArgentDawnInput, 9500);
-                setTimeout(RavencrestTable, 10000);
-                setTimeout(RavencrestInput, 10500);
-            }
-            window.onload = UpdateListings()
         }
-    }
-    // reload pages
-    function ReloadPages() {
-        if (location.pathname == Antonidas_pathname) {
-            console.log("Antonidas: перезагружаю страницу спустя 5 минут...")
-            location.reload();
-        } else if (location.pathname == ArgentDawn_pathname) {
-            console.log("Argent Dawn: перезагружаю страницу спустя 5 минут...")
-            location.reload();
-        } else if (location.pathname == Ravencrest_pathname) {
-            console.log("Ravencrest: перезагружаю страницу спустя 5 минут...")
-            location.reload();
-        } else {
-            console.log("Manage Listings: обновляю цены спустя 5 минут...")
-            UpdatePrices()
+        // таймер на выставление
+        function UpdateListings() {
+            setTimeout(AntonidasTable, 7000);
+            setTimeout(AntonidasInput, 7500);
+            setTimeout(ArgentDawnTable, 9000);
+            setTimeout(ArgentDawnInput, 9500);
+            setTimeout(RavencrestTable, 10000);
+            setTimeout(RavencrestInput, 10500);
         }
+        window.onload = UpdateListings()
+        setTimeout(UpdateListings, 300000);
     }
-    setTimeout(ReloadPages, 300000);
-})();
+// reload pages
+function ReloadPages() {
+    if (location.pathname == Antonidas_pathname) {
+        console.log("Antonidas: перезагружаю страницу спустя 5 минут...")
+        location.reload();
+    } else if (location.pathname == ArgentDawn_pathname) {
+        console.log("Argent Dawn: перезагружаю страницу спустя 5 минут...")
+        location.reload();
+    } else if (location.pathname == Ravencrest_pathname) {
+        console.log("Ravencrest: перезагружаю страницу спустя 5 минут...")
+        location.reload();
+    } 
+}
+setTimeout(ReloadPages, 300000);
+}) ();
