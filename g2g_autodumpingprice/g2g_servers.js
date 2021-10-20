4476200de6f3bc2f2723e67226cf57d8ea441597
Finegorko <Finegorko@gmail.com>
2021-10-21 00:37:17 +0300

    hey

--- g2g_autodumpingprice/g2g_servers.js
+++ g2g_autodumpingprice/g2g_servers.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         G2G Auto Dump Prices: Antonidas, Argent Dawn, Ravencrest
 // @namespace    http://g2g.com/
-// @version      0.76
+// @version      0.77
 // @author       Finegorko
 // @include      https://www.g2g.com/sell/manage?region=41709&service=1&game=2299&type=0&sorting=title%40asc
 // @include      https://www.g2g.com/offer/Antonidas*
